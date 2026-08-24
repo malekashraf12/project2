3d4d4b7fbf2a211b9577e3abd05c5523e026962f
malekashraf12 <hhjjkmalek@gmail.com>
2026-08-25 02:11:12 +0300

    Fix database connection for Vercel production

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const app = require('../app');
 const connectDB = require('../config/db');
 
-connectDB();
-
-module.exports = app;
+module.exports = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
--- config/db.js
+++ config/db.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      family: 4
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected');
   } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+    console.error('MongoDB Connection Error:', error);
   }
 };
 
